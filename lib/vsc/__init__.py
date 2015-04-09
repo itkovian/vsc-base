53a1710430bf382e87c538227fc56ad78a5f9a9d
Andy Georges <itkovian@gmail.com>
2015-04-09 16:35:32 +0200

    Move to recent way of doing stuff

--- lib/vsc/__init__.py
+++ lib/vsc/__init__.py
@@ -31,10 +31,8 @@ so explicitly declare this is also the vsc namespace
 
 @author: Jens Timmerman (Ghent University)
 """
-from pkgutil import extend_path
-
-# we're not the only ones in this namespace
-__path__ = extend_path(__path__, __name__)  #@ReservedAssignment
+import pkg_resources
+pkg_resources.declare_namespace(__name__)
 
 # here for backwards compatibility
 from vsc.utils import fancylogger
